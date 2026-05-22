f7ea054dd227ae529f94ab18483a7f2dd2dc51c1
zombaish <oscarbaish@gmail.com>
2026-05-22 11:10:30 +0100

    Add service worker for UV handling

---
+++ public/sw.js
@@ -0,0 +1,17 @@
+// public/sw.js
+importScripts('/uv/uv.bundle.js');
+importScripts('/uv/uv.config.js');
+importScripts('/uv/uv.sw.js');
+
+const uv = new UVServiceWorker();
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        (async () => {
+            if (event.request.url.startsWith(location.origin + __uv$config.prefix)) {
+                return await uv.fetch(event);
+            }
+            return await fetch(event.request);
+        })()
+    );
+});
